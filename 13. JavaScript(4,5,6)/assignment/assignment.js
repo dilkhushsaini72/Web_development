2cfa0f5b2ecbf4e54d1b28caeabf80f31d2a8dd2
Dilkhush saini <sainidilkhush019@gmail.com>
2026-02-05 21:28:47 +0530

    assignment

--- 13. JavaScript(4,5,6)/assignment/assignment.js
+++ 13. JavaScript(4,5,6)/assignment/assignment.js
@@ -1,1 +1,48 @@
-// Q.1
+// Q.1 Write a JavaScript function that returns array elements larger than a number.
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let num = 8;
+
+function greaterElements(arr, num) {
+  for (element of arr) {
+    if (element > num) {
+      console.log(element);
+    }
+  }
+}
+
+greaterElements(arr, num);
+
+// Q.2 Write a JavaScript funtion toextract unique characters from a string:
+
+let str = "abcdabcdfgggh";
+
+function getUnique(str) {
+  let ans = "";
+  for (let i = 0; i < str.length; i++) {
+    let currChar = str[i];
+    if (ans.indexOf(currChar) == -1) {
+      ans = ans + currChar;
+    }
+  }
+
+  return ans;
+}
+
+console.log(getUnique(str));
+
+// Q.3 Write a JavaScript function to count the number of vowels in a String argument.
+let str2 = "apna college";
+
+function vowelCount(str2) {
+  let count = 0;
+  for (ch of str2) {
+    if (ch == "a" || ch == "e" || ch == "i" || ch == "o" || ch == "u") {
+      count++;
+    }
+  }
+  console.log("no. of vowels : ", count);
+}
+
+vowelCount(str2);
+
+// Q. 5 Write a JavaScript function to generate a random number within a range (start, end).
