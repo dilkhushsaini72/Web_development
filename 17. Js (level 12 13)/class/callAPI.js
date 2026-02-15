dab450d6be9c668d30934ea1a9bc49dc4d208c31
Dilkhush saini <sainidilkhush019@gmail.com>
2026-02-15 21:12:41 +0530

    new api in level 12

--- 17. Js (level 12 13)/class/callAPI.js
+++ 17. Js (level 12 13)/class/callAPI.js
@@ -1,9 +1,16 @@
 let url = "https://catfact.ninja/fact?max_length=140";
 
 const getData = async () => {
+  try {
     let res = await fetch(url);
     let data = await res.json();
     console.log(data.fact);
-}
+    let res2 = await fetch(url);
+    let data2 = await res2.json();
+    console.log(data2.fact);
+  } catch (err) {
+    console.log("Error! : ", err);
+  }
+};
 
 getData();
