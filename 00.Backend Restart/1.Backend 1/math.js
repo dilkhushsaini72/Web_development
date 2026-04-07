555b57cd4e43d19abc56f4ca11d5b7e93b9a372f
Dilkhush saini <sainidilkhush019@gmail.com>
2026-04-07 15:46:02 +0530

    restarting backend

---
+++ 00.Backend Restart/1.Backend 1/math.js
@@ -0,0 +1,9 @@
+let pi = 3.14;
+let x = 24;
+let number = 1234;
+
+module.exports = {
+  pi,
+  x,
+  number,
+};
---
+++ 00.Backend Restart/1.Backend 1/script.js
@@ -0,0 +1,4 @@
+const val = require("./math");
+console.log(val.pi);
+console.log(val.x);
+console.log(val.number);
