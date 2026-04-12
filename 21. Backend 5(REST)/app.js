03b2fc18f3fb11d9abb034629b0c05e4941b2b80
Dilkhush saini <sainidilkhush019@gmail.com>
2026-04-12 21:29:12 +0530

    some modification

--- 21. Backend 5(REST)/app.js
+++ 21. Backend 5(REST)/app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
+app.get("/", (req,res) => {
+  res.send("Hello this is get request. ")
+})
+
 app.listen(PORT, () => {
   console.log(`server started on ${PORT}`);
 });
